2bd0d647e7ce4251eebc102856609e2048eef3d2
Amirhossein Foroughi <amirhosseinforoughi2@gmail.com>
2021-07-04 23:47:14 +0430

    roc added

--- roc.py
+++ roc.py
@@ -1,5 +1,136 @@
+from numpy.lib.function_base import append
 from sklearn.metrics import roc_curve                # Calculate the ROC curve
 import pandas as pd 
 
+# Import module for data manipulation
+import pandas as pd
+# Import module for linear algebra
+import numpy as np
+# Import module for data simulation
+from sklearn.datasets import make_classification     # Create a synthetic dataframe
+from sklearn.linear_model import LogisticRegression  # Classification model
+from sklearn.model_selection import train_test_split # Split the dataframe
+from sklearn.metrics import roc_curve                # Calculate the ROC curve
+from sklearn.metrics import precision_recall_curve   # Calculate the Precision-Recall curve
+from sklearn.metrics import f1_score                 # Calculate the F-score
+# Import module for data visualization
+from plotnine import *
+import plotnine
+
 results = pd.read_csv('result.csv')
-fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+y_test = results['label'].tolist()
+y_pred = results['predicted_label'].tolist()
+y_test_new = []
+y_pred_new = []
+
+for y in y_test:
+    if y == 'real':
+        y_test_new.append(0)
+    else:
+        y_test_new.append(1) 
+
+for y in y_pred:
+    if y == 'real':
+        y_pred_new.append(0)
+    else:
+        y_pred_new.append(1) 
+
+
+print(y_test_new)
+print(y_pred_new)
+fpr, tpr, thresholds = roc_curve(y_test_new, y_pred_new)
+# Plot the ROC curve
+df_fpr_tpr = pd.DataFrame({'FPR':fpr, 'TPR':tpr, 'Threshold':thresholds})
+df_fpr_tpr.head()
+
+# Create the data viz
+plotnine.options.figure_size = (8, 4.8)
+(
+    ggplot(data = df_fpr_tpr)+
+    geom_point(aes(x = 'FPR',
+                   y = 'TPR'),
+               size = 0.4)+
+    geom_line(aes(x = 'FPR',
+                  y = 'TPR'))+
+    labs(title = 'ROC Curve')+
+    xlab('False Positive Rate')+
+    ylab('True Positive Rate')+
+    theme_minimal()
+)
+
+gmean = np.sqrt(tpr * (1 - fpr))
+
+# Find the optimal threshold
+index = np.argmax(gmean)
+thresholdOpt = round(thresholds[index], ndigits = 4)
+gmeanOpt = round(gmean[index], ndigits = 4)
+fprOpt = round(fpr[index], ndigits = 4)
+tprOpt = round(tpr[index], ndigits = 4)
+print('Best Threshold: {} with G-Mean: {}'.format(thresholdOpt, gmeanOpt))
+print('FPR: {}, TPR: {}'.format(fprOpt, tprOpt))
+
+# Create data viz
+plotnine.options.figure_size = (8, 4.8)
+(
+    ggplot(data = df_fpr_tpr)+
+    geom_point(aes(x = 'FPR',
+                   y = 'TPR'),
+               size = 0.4)+
+    # Best threshold
+    geom_point(aes(x = fprOpt,
+                   y = tprOpt),
+               color = '#981220',
+               size = 4)+
+    geom_line(aes(x = 'FPR',
+                  y = 'TPR'))+
+    geom_text(aes(x = fprOpt,
+                  y = tprOpt),
+              label = 'Optimal threshold \n for class: {}'.format(thresholdOpt),
+              nudge_x = 0.14,
+              nudge_y = -0.10,
+              size = 10,
+              fontstyle = 'italic')+
+    labs(title = 'ROC Curve')+
+    xlab('False Positive Rate (FPR)')+
+    ylab('True Positive Rate (TPR)')+
+    theme_minimal()
+)
+
+# Calculate the Youden's J statistic
+youdenJ = tpr - fpr
+
+# Find the optimal threshold
+index = np.argmax(youdenJ)
+thresholdOpt = round(thresholds[index], ndigits = 4)
+youdenJOpt = round(gmean[index], ndigits = 4)
+fprOpt = round(fpr[index], ndigits = 4)
+tprOpt = round(tpr[index], ndigits = 4)
+print('Best Threshold: {} with Youden J statistic: {}'.format(thresholdOpt, youdenJOpt))
+print('FPR: {}, TPR: {}'.format(fprOpt, tprOpt))
+
+# Create a data viz
+plotnine.options.figure_size = (8, 4.8)
+(
+    ggplot(data = df_fpr_tpr)+
+    geom_point(aes(x = 'FPR',
+                   y = 'TPR'),
+               size = 0.4)+
+    # Best threshold
+    geom_point(aes(x = fprOpt,
+                   y = tprOpt),
+               color = '#981220',
+               size = 4)+
+    geom_line(aes(x = 'FPR',
+                  y = 'TPR'))+
+    # Annotate the text
+    geom_text(aes(x = fprOpt,
+                  y = tprOpt),
+              nudge_x = 0.14,
+              nudge_y = -0.10,
+              size = 10,
+              fontstyle = 'italic')+
+    labs(title = 'ROC Curve')+
+    xlab('False Positive Rate (FPR)')+
+    ylab('True Positive Rate (TPR)')+
+    theme_minimal()
+)
